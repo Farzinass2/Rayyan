ffa71342eb767a86a8436c153058693bab53c836
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 12:16:22 +0530

    kml

---
+++ Expt1/src/main/java/NumberFormat.java
@@ -0,0 +1,22 @@
+
+public class NumberFormat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			
+				String a="hello";
+				int b=Integer.parseInt(a);
+			}
+			catch(Exception e)
+			{
+				System.out.println("Exception handled");
+				System.out.println(e);
+			}
+			
+		}
+}
+
+	
+
+
