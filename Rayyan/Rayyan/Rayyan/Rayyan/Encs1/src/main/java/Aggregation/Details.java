6406e0a34339fb4ddb878bbe0c45b0b71a6046fc
Frazina <Frazina@LAPTOP-T5HRFLI1>
2025-01-06 12:06:05 +0530

    ftgh

---
+++ Rayyan/Rayyan/Rayyan/Rayyan/Encs1/src/main/java/Aggregation/Details.java
@@ -0,0 +1,15 @@
+package Aggregation;
+
+public class Details {
+	String city;
+	String state;
+	String country;
+
+
+Details(String city,String state,String country)
+{
+	this.city=city;
+	this.state=state;
+	this.country=country;
+}
+}
---
+++ Rayyan/Rayyan/Rayyan/Rayyan/Encs1/src/main/java/Ecapsulation/Employee2.java
@@ -0,0 +1,26 @@
+package Ecapsulation;
+
+public class Employee2 {
+	private String name="Elizebeth";
+	private int age=23;
+	private int id=12;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
